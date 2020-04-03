689392241bd65ba9037c71bd97852a73df776a12
Ubuntu <marshknow212@ubuntu1604.wdxwu0nhtotuto3g4nldxpnvve.gx.internal.cloudapp.net>
2020-04-03 21:33:45 +0000

    Added Perl file to directory

---
+++ Perl/nestedif.pl
@@ -0,0 +1,13 @@
+my $a=11; #Change values to 11,2,5 and observe output
+if($a<10){
+  print "Inside 1st if block";
+        if($a<5){
+                print "Inside 2nd if block --- The value is $a";
+        }
+        else{
+                print " Inside 2nd else block --- The value is $a";
+        }
+	}
+	else{
+        	print "Inside 1st else block – The value is $a";
+    }
