ce4e5c8014c6aa880e53c12662c1c73e6d69f19e
Ubuntu <marshknow212@ubuntu1604.wdxwu0nhtotuto3g4nldxpnvve.gx.internal.cloudapp.net>
2020-04-03 21:40:13 +0000

    Added Ruby file to the directory

---
+++ Ruby/printarray.rb
@@ -0,0 +1,13 @@
+array1 = ["Ruby", "Perl", "C++", "Java", "Python"]
+puts "Unix can be programmed in all these languages: "
+puts " "
+for array_element in array1
+          puts array_element
+end
+TIME = [Time.now]
+puts " "
+puts "The current time is: "
+puts TIME
+puts " "
+
+
